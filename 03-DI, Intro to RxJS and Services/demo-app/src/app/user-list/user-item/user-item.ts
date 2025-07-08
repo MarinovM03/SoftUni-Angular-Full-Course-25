16b5ea7b4440b1049f7efa4e1c09720c6c3f1935
Martin Marinov <martin.marinov10@yahoo.com>
2025-07-09 01:03:45 +0400

    New component created

---
+++ 03-DI, Intro to RxJS and Services/demo-app/src/app/user-list/user-item/user-item.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-item.html',
+  styleUrl: './user-item.css'
+})
+export class UserItem {
+
+}
