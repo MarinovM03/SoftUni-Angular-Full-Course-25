28e61655e3e14b079e4ef0112a8791e12d76dbfc
Martin Marinov <martin.marinov10@yahoo.com>
2025-07-09 18:38:42 +0400

    Update

--- 03-DI, Intro to RxJS and Services/demo-app/src/app/solid-demo.ts
+++ 03-DI, Intro to RxJS and Services/demo-app/src/app/solid-demo.ts
@@ -79,7 +79,7 @@ class Car {
     model: string;
     color: string;
 
-    constructor(model: string, color: string;) {
+    constructor(model: string, color: string) {
         this.model = model;
         this.color = color;
     }
