fedaa6519c9e6852eeeb504efda3967c92dc5f17
Martin Marinov <martin.marinov10@yahoo.com>
2025-07-11 05:29:40 +0400

    Move logic into the user service

--- 03-DI, Intro to RxJS and Services/demo-app/src/app/app.ts
+++ 03-DI, Intro to RxJS and Services/demo-app/src/app/app.ts
@@ -2,40 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserList } from './user-list/user-list';
 import { SimpleUser } from './types';
+import { UserService } from './user-list/user-service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, UserList],
+  // providers: [],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'demo-app';
 
-  appUsers: SimpleUser[] = [
-    { name: 'Martin', age: 21},
-    { name: 'Ivo', age: 34},
-    { name: 'Peter', age: 42},
-  ];
+  constructor(public userService: UserService) {
+
+  }
 
   handleClick() {
     this.title = 'New text';
-  }
 
-  addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
-    const user = {
-      name: inputName.value,
-      age: Number(inputAge.value),
-    };
-
-    this.appUsers.push(user);
-    // this.appUsers = [...this.appUsers, user];  //! Important `HACK` we can use
-
-    inputName.value = '';
-    inputAge.value = '';
-
-    console.log(this.appUsers);
-    
   }
 }
--- 03-DI, Intro to RxJS and Services/demo-app/src/app/user-list/user-service.ts
+++ 03-DI, Intro to RxJS and Services/demo-app/src/app/user-list/user-service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { SimpleUser } from '../types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  appUsers: SimpleUser[] = [
+    { name: 'Martin', age: 21},
+    { name: 'Ivo', age: 34},
+    { name: 'Peter', age: 42},
+  ];
+
+  addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
+    const user = {
+      name: inputName.value,
+      age: Number(inputAge.value),
+    };
+
+    this.appUsers.push(user);
+    // this.appUsers = [...this.appUsers, user];  //! Important `HACK` we can use
+
+    inputName.value = '';
+    inputAge.value = '';
+
+    console.log(this.appUsers);
+    
+  }
 
   constructor() { }
 }
