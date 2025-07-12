a9e9c97d068ed7f4ce71359cfc3f61c3f5bcb8d6
Martin Marinov <martin.marinov10@yahoo.com>
2025-07-12 17:47:58 +0400

    Add observable example

--- 03-DI, Intro to RxJS and Services/demo-app/src/promise-vs-observables.ts
+++ 03-DI, Intro to RxJS and Services/demo-app/src/promise-vs-observables.ts
@@ -1,3 +1,5 @@
+import { Observable } from "rxjs";
+
 // PROMISES
 // - Promise - are not lazy, code invokes!
 // - Can't be cancelled!
@@ -11,10 +13,29 @@ const p = new Promise((resolve, reject) => {
   }, 3000);
 });
 
-p.then((data) => console.log(data))
+p.then((data) => console.log('Promise: ', data))
   .then()
   .then()
   .catch(e => console.error(e));
   
 // Sync analogy
-[1].map(x => x * 2).map(y => y * 100);
+[1].map((x) => x * 2).map((y) => y * 100);
+
+
+// Observables
+
+// Sync Analogy
+[1, 2, 3, 4].map((x) => x * 2).map((y) => y * 100);
+
+
+const o = new Observable((observer) => {
+   setTimeout(() => {
+    observer.next(101);
+    observer.next(102);
+    observer.next(103);
+   }, 2000);
+});
+
+o.subscribe((data) => {
+    console.log('from observable: ', data);
+});
