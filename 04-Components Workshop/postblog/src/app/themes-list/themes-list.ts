8272b0433705807a39575299e95eaf1fb692a84a
Martin Marinov <martin.marinov10@yahoo.com>
2025-07-14 22:28:43 +0400

    Add themes-list component

---
+++ 04-Components Workshop/postblog/src/app/themes-list/themes-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-themes-list',
+  imports: [],
+  templateUrl: './themes-list.html',
+  styleUrl: './themes-list.css'
+})
+export class ThemesListComponent {
+
+}
