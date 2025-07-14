742391d3fbbd76c8d62beb9a97c21cdd02d784ca
Martin Marinov <martin.marinov10@yahoo.com>
2025-07-14 22:34:54 +0400

    Import ThemesListComponent and PostsListComponent in main

--- 04-Components Workshop/postblog/src/app/main/main.ts
+++ 04-Components Workshop/postblog/src/app/main/main.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { ThemesListComponent } from '../themes-list/themes-list';
+import { PostsListComponent } from '../posts-list/posts-list';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [
+    ThemesListComponent,
+    PostsListComponent
+  ],
   templateUrl: './main.html',
   styleUrl: './main.css'
 })
