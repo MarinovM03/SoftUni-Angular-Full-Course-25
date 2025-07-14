3d66c894859864ce7f29f37e24a3554382feda42
Martin Marinov <martin.marinov10@yahoo.com>
2025-07-15 00:28:23 +0400

    Fetch data using api service

--- 04-Components Workshop/postblog/src/app/posts-list/posts-list.ts
+++ 04-Components Workshop/postblog/src/app/posts-list/posts-list.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api';
 
 @Component({
   selector: 'app-posts-list',
@@ -6,6 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './posts-list.html',
   styleUrl: './posts-list.css'
 })
-export class PostsListComponent {
+export class PostsListComponent implements OnInit {
+  constructor(private apiService: ApiService) {}
 
+  ngOnInit(): void {
+    this.apiService.getPosts().subscribe((p) => {
+      console.log(p);
+    });
+  }
 }
