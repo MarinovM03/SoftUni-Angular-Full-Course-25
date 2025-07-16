d7171f60de8770da1038f685e9e20c4a5c4e4f7e
Martin Marinov <martin.marinov10@yahoo.com>
2025-07-16 04:49:33 +0400

    Update Post interface using imports from User and Theme interfaces

--- 04-Components Workshop/postblog/src/app/types/post.ts
+++ 04-Components Workshop/postblog/src/app/types/post.ts
@@ -1,4 +1,13 @@
+import { Theme } from "./themes";
+import { User } from "./user";
+
 export interface Post {
-    title: string;
-    content: string;
+    likes: string[];
+    _id: string;
+    text: string;
+    userId: User;
+    themeId: Theme;
+    created_at: string;
+    updatedAt: string;
+    __v: number;
 }
