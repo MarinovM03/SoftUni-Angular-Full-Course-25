ea43ad7438353b9c4d75fb4d2241bb19fccd64bb
Martin Marinov <martin.marinov10@yahoo.com>
2025-07-17 22:19:17 +0400

    Add loader on themes-list

--- 04-Components Workshop/postblog/src/app/themes-list/themes-list.ts
+++ 04-Components Workshop/postblog/src/app/themes-list/themes-list.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api';
 import { Theme } from '../types/themes';
+import { LoaderComponent } from '../shared/loader/loader';
 
 @Component({
   selector: 'app-themes-list',
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './themes-list.html',
   styleUrl: './themes-list.css'
 })
 export class ThemesListComponent implements OnInit {
   themes: Theme[] = [];
 
+  isLoading = true;
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
     this.apiService.getThemes().subscribe((themes) => {
       this.themes = themes;
+      this.isLoading = false;
     });
   }
 }
