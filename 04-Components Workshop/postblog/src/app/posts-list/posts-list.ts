7246a2850c9708115e085695baa586c386cbb2ff
Martin Marinov <martin.marinov10@yahoo.com>
2025-07-17 22:22:32 +0400

    Add loader on posts-list

--- 04-Components Workshop/postblog/src/app/posts-list/posts-list.ts
+++ 04-Components Workshop/postblog/src/app/posts-list/posts-list.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api';
 import { Post } from '../types/post';
+import { LoaderComponent } from '../shared/loader/loader';
 
 @Component({
   selector: 'app-posts-list',
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './posts-list.html',
   styleUrl: './posts-list.css'
 })
 export class PostsListComponent implements OnInit {
   posts: Post[] = [];
 
+  isLoading = true;
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getPosts(5).subscribe((posts) => {
       this.posts = posts;
+      this.isLoading = false;
     });
   }
 }
